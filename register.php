c403fc39013c0d19861c365b25d9945c90b3837a
Stu <sgold8670@gmail.com>
2015-02-04 00:35:57 -0500

    first commit

---
+++ register.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Bootstrap Advanced Web Development</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<!-- Google fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,700italic,700,400italic,300italic,300' rel='stylesheet' type='text/css'>
+	<!-- Bootstrap Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<!-- Bootstrap Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+</head>
+
+<body id="register">
+	<section class="container">
+    	<div class="content row">
+        	<section class="main col col-lg-8">
+            	
+            </section><!-- main -->
+            <section class="sidebar col col-lg-4">
+            	
+            </section><!-- sidebar -->
+        </div><!-- end of content/row -->
+    </section><!-- end container -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
